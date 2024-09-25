d1906c67ab124e431d962235edd5a75026b4303c
JoaoSant7 <petrvsioannes@gmail.com>
2024-09-25 10:42:30 -0300

    criei o repositório

---
+++ licao.js
@@ -0,0 +1,41 @@
+/*rotulo = "atividade"
+const nome = "John"
+let idade = "21"
+console.log ('Me chamo John. Tenho 21 anos');/*/
+
+/*const nome = "Casa Amarela"
+const cidade = "Recife"
+console.log ('Casa Amarela se localiza em Recife');/*/
+
+/*const numero1 = 40;
+const numero2 = 2;
+let soma = 40 + 2;
+let subtração = 40 - 2;
+let multiplicação = 70 * 33;
+let divisão = 70 / 33;/*/
+
+/*function areaRetangulo(base, altura) {
+    return base * altura;
+}
+
+const base = 12;
+const altura = 40;
+console.log("Área do retângulo:", areaRetangulo(base, altura));/*/
+
+/*function calcularMedia(num1, num2, num3) {
+    const soma = num1 + num2 + num3;
+    const media = soma / 3;
+    return media;
+}
+const numero1 = 10;
+const numero2 = 7;
+const numero3 = 5;
+
+const media = calcularMedia (numero1, numero2, numero3);
+console.log("Média igual a", media);/*/
+
+/*var valor = (200);
+var percentualdesconto = (50);
+var valorfinal = valorfinal = valor - ( valor * (percentualdesconto / 100));
+console.log (`"O valor final do desconto é" ${valorfinal}$`);/*/
+
