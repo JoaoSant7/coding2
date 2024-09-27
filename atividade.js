3e6556bc81f7f776e2187b1d1b53557df427531c
João Pedro do Nascimento Santana <petrvsioannes@gmail.com>
2024-09-27 10:47:36 -0300

    Create atividade.js

---
+++ atividade.js
@@ -0,0 +1,77 @@
+/*rotulo = "atividade"
+const nome = "John"
+let idade = "21"
+console.log ('Me chamo John. Tenho 21 anos');/*/
+
+/*const nome = "Casa Amarela"
+const cidade = "Recife"
+console.log ('Casa Amarela se localiza em Recife');/*/
+
+//4 Operações básicas
+/*const numero1 = 40;
+const numero2 = 2;
+let soma = 40 + 2;
+let subtração = 40 - 2;
+let multiplicação = 70 * 33;
+let divisão = 70 / 33;/*/
+
+/*function areaRetangulo(base, altura) {
+    return base * altura;
+}
+
+//Calculadora de área do retângulo
+const base = 12;
+const altura = 40;
+console.log("Área do retângulo:", areaRetangulo(base, altura));/*/
+
+//Calculadora de média com três números
+/*function calcularMedia(num1, num2, num3) {
+    const soma = num1 + num2 + num3;
+    const media = soma / 3;
+    return media;
+}
+const numero1 = 10;
+const numero2 = 7;
+const numero3 = 5;
+
+const media = calcularMedia (numero1, numero2, numero3);
+console.log("Média igual a", media);/*/
+
+//Calculadora de desconto
+/*var valor = (200);
+var percentualdesconto = (50);
+var valorfinal = valorfinal = valor - ( valor * (percentualdesconto / 100));
+console.log (`"O valor final do desconto é" ${valorfinal}$`);/*/
+
+//Conversor de moedas com taxa de câmbio
+/*const taxasDeCambio = {
+    'USD': { 'BRL': 5.25, 'EUR': 0.85 },
+    'BRL': { 'USD': 0.19, 'EUR': 0.16 },
+    'EUR': { 'USD': 1.18, 'BRL': 6.25 }
+};
+
+function converterMoeda(valor, moedaInicial, moedaFinal) {
+    const taxa = taxasDeCambio[moedaInicial][moedaFinal];
+    return valor * taxa;
+}
+
+const valor = 200;
+const resultado = converterMoeda(valor, 'USD', 'BRL');
+console.log(`${valor} dólares valem ${resultado.toFixed(2)} reais brasileiros`);/*/
+
+//Conversor de Celsius-Fahrenheit
+/*let celsius = 25;
+let fahrenheit = (celsius * 9 / 5) + 32;
+
+
+console.log(`${celsius}°C em Fahrenheit é igual a ${fahrenheit.toFixed(2)}°F.`);/*/
+
+
+//Calculadora de IMC
+/*let peso = 110;
+let altura = 2.10; 
+
+
+let imc = peso / (altura * altura);
+
+console.log(`Seu IMC é ${imc.toFixed(2)}.`);/*/
