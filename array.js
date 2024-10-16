59a8572bd43ca1198127b7c59102186e9c8e3e42
João Pedro do Nascimento Santana <petrvsioannes@gmail.com>
2024-10-16 10:32:31 -0300

    Update array.js

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 //ARRAY
 
 const livros = ["Catecismo da Igreja Católica", "História de uma Alma", "Castelo Interior"];
-console.log(livros[0] [2]); //Este código encontra o livro (elemento) a partir do index (posição)
+console.log(livros[1] [0]); //Este código encontra o livro (elemento) a partir do index (posição)
 
 //Segundo colchete pesquisa pela letra do elemento selecionado, considerando o index (posição) da letra
 
@@ -10,7 +10,11 @@ console.log(consulta) //Este código encontra o index (posição) a partir do li
 
 console.log(livros[1][1]); //Outra forma de pesquisar pela letra do elemento a partir do index
 
+console.log(livros.pop()); //remove o último elemento e mostra o elemento removido
 
+console.log(livros.push("Confissões")); //adiciona elementos no final da array e mosta a quantidade do array
+
+console.log(livros);
 
 //Elementos: valores que ficam agrupados dentro do array
 //Cada elemento da array está em uma posição; esta posição é o INDEX -> 0, 1, 2, 3 ...
