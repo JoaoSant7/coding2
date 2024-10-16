99f7927044f7b6a832b109f549250d074beaa116
João Pedro do Nascimento Santana <petrvsioannes@gmail.com>
2024-10-16 10:02:27 -0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,12 @@
+//ARRAY
+
+const livros = ["Javascript Assertivo", "ECMAScript 6", "MongoDB"];
+console.log(livros[0]); //Este código encontra o livro (elemento) a partir do index (posição)
+
+let consulta = livros.indexOf("Javascript Assertivo")
+console.log(consulta) //Este código encontra o index (posição) a partir do livro (elemento)
+
+
+
+//Elementos: valores que ficam agrupados dentro do array
+//Cada elemento da array está em uma posição; esta posição é o INDEX -> 0, 1, 2, 3 ...
