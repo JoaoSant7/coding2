43325470f1062a35fd42f398f68be780a04abb65
João Santana <petrvsioannes@gmail.com>
2024-10-22 08:56:46 -0300

    Create default.js

---
+++ default.js
@@ -0,0 +1,13 @@
+let fruit = 'banana';
+
+switch (fruit) {
+    case 'apple':
+        console.log('Apple selected');
+        break;
+    case 'orange':
+        console.log('Orange selected');
+        break;
+    default:
+        console.log('No fruit selected');
+        break;
+}
