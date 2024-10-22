d2050c1f33528ae4c380a2bad60d2a995d65e9e3
João Santana <petrvsioannes@gmail.com>
2024-10-22 09:02:58 -0300

    Update default.js

--- default.js
+++ default.js
@@ -1,3 +1,7 @@
+//No JavaScript, o switch é uma estrutura de controle que permite executar diferentes partes de código com base no valor de uma expressão. O default é uma cláusula opcional que pode ser usada para lidar com casos que não correspondem a nenhum dos case especificados.
+
+//Aqui está um exemplo básico:
+
 let fruit = 'banana';
 
 switch (fruit) {
@@ -12,3 +16,5 @@ switch (fruit) {
         console.log('No fruit selected');
         break;
 }
+
+//Nesse exemplo, como fruit é 'banana', a saída será "No fruit selected", pois não há um case correspondente. O default atua como uma opção de "fallback" quando nenhuma das outras condições é atendida.
